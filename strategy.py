7c3d95871c7d1c5a7c28a7620b204f9616e94216
shobanshone <106527322+shobanshone@users.noreply.github.com>
2026-02-03 21:08:01 +0530

    Refactor order placement logic in execute_iron_fly

--- strategy.py
+++ strategy.py
@@ -15,47 +15,60 @@ kite.set_access_token(os.environ["KITE_ACCESS_TOKEN"])
 
 EXPIRIES = {
     "NIFTY": {
-    "03 Feb 2026": "03FEB",
-    "10 Feb 2026": "10FEB",
-    "17 Feb 2026": "17FEB",
-    "24 Feb 2026": "24FEB",
-    "02 Mar 2026": "02MAR",
-    "30 Mar 2026": "30MAR",
-    "28 Apr 2026": "28APR",
-    "30 Jun 2026": "30JUN",
-    "29 Sep 2026": "29SEP",
-    "29 Dec 2026": "29DEC",
-    "29 Jun 2027": "29JUN",
-    "28 Dec 2027": "28DEC",
-    "27 Jun 2028": "27JUN",
-    "26 Dec 2028": "26DEC",
-    "26 Jun 2029": "26JUN",
-    "24 Dec 2029": "24DEC",
-    "25 Jun 2030": "25JUN",
-    "31 Dec 2030": "31DEC"
+    # Weekly expiries (YYMDD)
+    "10 Feb 2026": "26210",
+    "17 Feb 2026": "26217",
+    "24 Feb 2026": "26FEB",
+    "02 Mar 2026": "26302",
+
+    # Month-end expiries (YYMMM)
+    "30 Mar 2026": "26MAR",
+    "28 Apr 2026": "26APR",
+    "30 Jun 2026": "26JUN",
+    "29 Sep 2026": "26SEP",
+    "29 Dec 2026": "26DEC",
+
+    "29 Jun 2027": "27JUN",
+    "28 Dec 2027": "27DEC",
+
+    "27 Jun 2028": "28JUN",
+    "26 Dec 2028": "28DEC",
+
+    "26 Jun 2029": "29JUN",
+    "24 Dec 2029": "29DEC",
+
+    "25 Jun 2030": "30JUN",
+    "31 Dec 2030": "30DEC"
+
 },
     "SENSEX": {
-    "05 Feb 2026": "05FEB",
-    "12 Feb 2026": "12FEB",
-    "19 Feb 2026": "19FEB",
+   
+    "05 Feb 2026": "26205",
+    "12 Feb 2026": "26212",
+    "19 Feb 2026": "26219",
     "26 Feb 2026": "26FEB",
-    "05 Mar 2026": "05MAR",
-    "12 Mar 2026": "12MAR",
-    "19 Mar 2026": "19MAR",
-    "25 Mar 2026": "25MAR",
-    "30 Apr 2026": "30APR",
-    "25 Jun 2026": "25JUN",
-    "24 Sep 2026": "24SEP",
-    "31 Dec 2026": "31DEC",
-    "24 Jun 2027": "24JUN",
-    "30 Dec 2027": "30DEC",
-    "29 Jun 2028": "29JUN",
+
+    "05 Mar 2026": "26305",
+    "12 Mar 2026": "26312",
+    "19 Mar 2026": "26319",
+    "25 Mar 2026": "26MAR",
+    "30 Apr 2026": "26APR",
+
+    "25 Jun 2026": "26JUN",
+    "24 Sep 2026": "26SEP",
+
+    "24 Jun 2027": "27JUN",
+    "30 Dec 2027": "27DEC",
+
+    "29 Jun 2028": "28JUN",
     "28 Dec 2028": "28DEC",
-    "28 Jun 2029": "28JUN",
-    "27 Dec 2029": "27DEC",
-    "27 Jun 2030": "27JUN",
-    "26 Dec 2030": "26DEC"
+
+
+    # Month-end expiries → YYMMM
+    "31 Dec 2026": "26DEC"
 }
+
+
 }
 
 
@@ -71,13 +84,21 @@ INDEX_CONFIG = {
 }
 
 
+def get_kite_user_name():
+    try:
+        profile = kite.profile()
+        return profile.get("user_name") or profile.get("user_id")
+    except Exception:
+        return None
+
 def execute_iron_fly(
     index_name,
     atm,
     hedge_distance,
     expiry_key,
     lots,
-    order_type
+    order_type,
+    dry_run=False
 ):
     config = INDEX_CONFIG[index_name]
     lot_size = config["lot_size"]
@@ -89,21 +110,37 @@ def execute_iron_fly(
     ce_buy = atm + hedge_distance
     pe_buy = atm - hedge_distance
 
+    orders = []
+
     def place(tradingsymbol, transaction):
-        
-        kite.place_order(
-             variety=kite.VARIETY_REGULAR,
-             exchange=exchange,
-             tradingsymbol=tradingsymbol,
-             transaction_type=transaction,
-             quantity=qty,
-             product=kite.PRODUCT_NRML,
-             order_type=order_type
-         )
+        if dry_run:
+            return {
+                "variety" : kite.VARIETY_REGULAR,
+                "exchange" : exchange,
+                "symbol": tradingsymbol,
+                "transaction": transaction,
+                "qty": qty,
+                "product" : kite.PRODUCT_NRML,  
+                "order_type": order_type
+            }
+        else:
+            order_id = kite.place_order(
+                variety=kite.VARIETY_REGULAR,
+                exchange=exchange,
+                tradingsymbol=tradingsymbol,
+                transaction_type=transaction,
+                quantity=qty,
+                product=kite.PRODUCT_NRML,
+                order_type=order_type
+            )
+            return order_id
 
-    place(f"{index_name}{expiry}{ce_buy}CE", "BUY")
-    place(f"{index_name}{expiry}{pe_buy}PE", "BUY")
-    place(f"{index_name}{expiry}{atm}PE", "SELL")
-    place(f"{index_name}{expiry}{atm}CE", "SELL")
+    # BUY hedges
+    orders.append(place(f"{index_name}{expiry}{ce_buy}CE", "BUY"))
+    orders.append(place(f"{index_name}{expiry}{pe_buy}PE", "BUY"))
 
+    # SELL ATM
+    orders.append(place(f"{index_name}{expiry}{atm}PE", "SELL"))
+    orders.append(place(f"{index_name}{expiry}{atm}CE", "SELL"))
 
+    return orders
